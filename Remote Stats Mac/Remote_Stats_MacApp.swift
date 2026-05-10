48a5250ffec5631d89dac57fe0ca507cc027eb14
Curtis Netterville <curtis@netterville.com>
2026-05-09 19:09:28 -0500

    Initial Commit

---
+++ Remote Stats Mac/Remote_Stats_MacApp.swift
@@ -0,0 +1,17 @@
+//
+//  Remote_Stats_MacApp.swift
+//  Remote Stats Mac
+//
+//  Created by Curtis Netterville on 5/9/26.
+//
+
+import SwiftUI
+
+@main
+struct Remote_Stats_MacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
